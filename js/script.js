c770f5aec66cd2c7f1cb4fa69db80d36057c7160
Paulo Victor <paulpbi16@gmail.com>
2022-11-16 16:14:05 -0300

    feat: adicionado troca de fotos na galeria de imagem

--- js/script.js
+++ js/script.js
@@ -42,3 +42,21 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// troca de imagens
+const galeria = document.querySelectorAll(".bicicleta-imagem img");
+const galeriaContainer = document.querySelector(".bicicleta-imagem");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
